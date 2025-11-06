4163e356482bd50b5d7be097c141bf2f8d8a5ce5
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-06 04:34:05 +0000

    Updated task_1/js/main.ts with examples

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,32 @@
 interface Teacher {
-    firstName: string,
-    lastName: string,
+    readonly firstName: string, // modifiable only after initialized
+    readonly lastName: string, // modifiable only after initialized
     fullTimeEmployee: boolean,
-    yearsOfExperience: number,
+    yearsOfExperience?: number, // optional
     location: string,
-    contract: boolean
+    [key: string]: any; // For the posibility to add extra attributes
 }
+
+// Example 1
+const teacher3: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "London",
+    contract: false, // this extra attribute was not listed above (no typed error)
+
+    // NB: yearsOfExperience attribute was excluded but no typed error because it was set to optional (?)
+}
+
+// Example 2
+const teacher4: Teacher = {
+  firstName: "James",
+  lastName: "Agalga",
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: "Accra",
+  contract: false,
+  department: "Science" // another extra attribute
+};
+
+console.log(teacher3)
